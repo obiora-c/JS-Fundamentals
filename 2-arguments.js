0ac28f8042000a59ce5505ebcda71c50fc2e188e
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2025-11-08 05:38:00 -0800

    intro to javascript

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,19 +2,10 @@
 
 const args = process.argv.slice(2);
 
-function checkArguments(...A){
-
-    if(A.length === 0){
-        console.log("No argument");
-
-    } else if (A.length === 1){
-        console.log("Argument found");
-    }else {
-        console.log("Arguments found");
-    }
-
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-
-checkArguments();
-checkArguments("Hello");
-checkArguments("Tunde" , "Fola");
